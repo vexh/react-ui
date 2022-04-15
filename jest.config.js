8858e0ce5b4e6cbdc6b26040a2770d77a96b17e8
wuhao <wuhao8882@gmail.com>
2022-04-15 21:53:44 +0800

    jest: handling static assets

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    "\\.svg$": "<rootDir>/__mocks__/svg.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
   },
 };
