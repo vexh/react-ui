6cfabc29782ae27184f4f9f9eda3c73b2daff811
wuhao <wuhao8882@gmail.com>
2022-04-15 16:25:47 +0800

    icons: 添加 className 属性

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import wechat from './icons/wechat.svg';
 import alipay from "./icons/alipay.svg";
 import qq from "./icons/qq.svg";
+import classes from '../helpers/classes';
 
 console.log(wechat);
 console.log(alipay);
@@ -14,14 +15,12 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = ({ name, ...restProps }) => {
-  return (
-    <>
-      <svg {...restProps}>
-        <use xlinkHref={`#${name}`}></use>
-      </svg>
-    </>
-  );
-};
+const Icon: React.FunctionComponent<IconProps> = ({ name, className, ...restProps }) => {
+  return <>
+    <svg className={classes('react-ui', className)} {...restProps}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
+  </>
+}
 
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import Button from './button';
 import Icon from './icon';
+
+
 const container = document.getElementById('app') as HTMLElement; // 必须确保 container 不是空
 // import Button from './Button';
 // export default function Index() {
