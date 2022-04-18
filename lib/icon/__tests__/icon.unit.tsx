06c382785d5f18c2b5a89ddf30a74df3691588c1
wuhao <wuhao8882@gmail.com>
2022-04-18 13:47:30 +0800

    chore

--- lib/icon/__tests__/icon.unit.tsx
+++ lib/icon/__tests__/icon.unit.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Icon from '../icon';
 
-test('Icon 是个 svg', () => {
-    const component = renderer.create(
-        <Icon name="wechat" />,
-    );
+describe('Icon 组件', () => {
+    it('成功渲染', () => {
+        const component = renderer.create(
+            <Icon name="wechat" />,
+        );
+    
+        expect(component).toMatchSnapshot();
+    })
 
-    expect(component).toMatchSnapshot();
+    it('on click', () => {
+        // const fn = jest.fn();
+        // const component = mount(<Icon name="wechat" onClick={fn}></Icon>)
+        // component.find('svg').simulate('click');
+        // expect(fn).toBeCalled();
+    })
 })
