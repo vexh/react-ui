385c6e7add400c4fa414ec741059add10c413e3f
wuhao <wuhao8882@gmail.com>
2022-04-18 16:16:06 +0800

    Chore: ci test

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,12 @@
+export { default as Icon } from './icon/icon';
+
 // After
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-// import Button from './button';
-import Icon from './icon/icon';
-
-
-const container = document.getElementById('app') as HTMLElement; // 必须确保 container 不是空
-// import Button from './Button';
-// export default function Index() {
-//     return <div>xxxx</div>
-// }
-createRoot(container).render(<Icon name="wechat" onClick={() => {console.log('click')}} onMouseEnter={() => {console.log('onMouseEnter')}} onMouseLeave={() => {console.log('onMouseLeave')}} />)
+// import React from 'react';
+// import Icon from './icon/icon';
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app') as HTMLElement; // 必须确保 container 不是空
+// // import Button from './Button';
+// // export default function Index() {
+// //     return <div>xxxx</div>
+// // }
+// createRoot(container).render(<Icon name="wechat" onClick={() => {console.log('click')}} onMouseEnter={() => {console.log('onMouseEnter')}} onMouseLeave={() => {console.log('onMouseLeave')}} />)
