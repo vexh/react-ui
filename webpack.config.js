f080267c16475d6ac18c6559335941cf2a264f01
wuhao <wuhao8882@gmail.com>
2022-04-21 16:17:26 +0800

    处理 png 文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,70 @@
 // 此处运行时是 Node.js 环境
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    // mode: "production",
+  // mode: "production",
 
-    entry: {
-        index: './lib/index.tsx'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist/lib'),
-        library: "react-ui",
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-            {
-                test: /\.svg/,
-                loader: 'svg-sprite-loader',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  // 将 JS 字符串生成为 style 节点
-                  'style-loader',
-                  // 将 CSS 转化成 CommonJS 模块
-                  'css-loader',
-                  // 将 Sass 编译成 CSS
-                  'sass-loader',
-                ],
-              },
-        
-        ]
-    },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         title: 'react-ui',
-    //         template: 'index.html'
-    //     })
-    // ],
-    // externals: {
-    //     react: {
-    //       commonjs: 'react',
-    //       commonjs2: 'react',
-    //       amd: 'react',
-    //       root: 'React',
-    //     },
-    //     'react-dom': {
-    //       commonjs: 'react-dom',
-    //       commonjs2: 'react-dom',
-    //       amd: 'react-dom',
-    //       root: 'ReactDOM',
-    //     },
-    // }
-}
+  entry: {
+    index: "./lib/index.tsx",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "react-ui",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.svg/,
+        loader: "svg-sprite-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          "style-loader",
+          // 将 CSS 转化成 CommonJS 模块
+          "css-loader",
+          // 将 Sass 编译成 CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  // plugins: [
+  //     new HtmlWebpackPlugin({
+  //         title: 'react-ui',
+  //         template: 'index.html'
+  //     })
+  // ],
+  // externals: {
+  //     react: {
+  //       commonjs: 'react',
+  //       commonjs2: 'react',
+  //       amd: 'react',
+  //       root: 'React',
+  //     },
+  //     'react-dom': {
+  //       commonjs: 'react-dom',
+  //       commonjs2: 'react-dom',
+  //       amd: 'react-dom',
+  //       root: 'ReactDOM',
+  //     },
+  // }
+};
