08e622ec5a80af82acc7f727f8f421b8d5793437
wuhao <wuhao8882@gmail.com>
2022-04-26 16:43:49 +0800

    Scroll 组件: 创建滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,5 +1,5 @@
 import classes from "../helpers/classes";
-import React, { HTMLAttributes } from "react";
+import React, { EventHandler, HTMLAttributes, UIEventHandler, useEffect, useRef, useState } from "react";
 import scrollbarWidth from "./scrollbar-width";
 import "./scroll.scss";
 
@@ -7,10 +7,27 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const { className, ...rest } = props;
+  const [barHeight, setBarHeight] = useState<number>();
+  const container = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+
+    const h = container.current?.getBoundingClientRect().height || 1;
+    const h2 = container.current?.scrollHeight || 1;
+    
+    setBarHeight((h * h) / h2);
+  }, []);
+  console.log({barHeight});
+  // const getHeight: UIEventHandler = (e) => {
+  //   console.log("scrollTop", e.currentTarget.scrollTop);
+  //   console.log("scrollHeight", e.currentTarget.scrollHeight);
+  // };
   return (
     <div className={classes("deepin-scroll", props.className)} {...rest}>
-      <div className="deepin-scroll-inner" style={{right: -scrollbarWidth()}}>
+      <div className="deepin-scroll-inner" style={{ right: -scrollbarWidth() }} ref={container} onScroll={() => {}}>
         {props.children}
+      </div>
+      <div className="deepin-scroll-track">
+        <div className="deepin-scroll-bar" style={{ height: `${barHeight}px` }}></div>
       </div>
     </div>
   );
